ed0c3af9878032cdad184e0be4b59485fc1b1fdf
Carlos Chaucanes - @Softimus <cachaucanes23@gmail.com>
2026-05-19 15:50:10 -0500

    fix: remove CREATE DATABASE from db.sql to use Docker-created database

--- database/db.sql
+++ database/db.sql
@@ -1,12 +1,5 @@
--- creating the database
 
-CREATE DATABASE crudnodejsmysql;
-
--- using the database
-
-use crudnodejsmysql;
-
-CREATE TABLE customer (
+CREATE TABLE IF NOT EXISTS customer (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   address VARCHAR(100) NOT NULL,
